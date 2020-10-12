a8a2d7a0495f4485c8b4507b4a2f90085c7dbef1
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-10-12 13:30:28 +0100

    Connec to database

--- src/config/database/database.module.ts
+++ src/config/database/database.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'bnext-contact-database',
+      port: 3306,
+      username: 'test',
+      password: 'test',
+      database: 'contact-book',
+      entities: [],
+      synchronize: true,
+    }),
+  ]
+})
 export class DatabaseModule {}
