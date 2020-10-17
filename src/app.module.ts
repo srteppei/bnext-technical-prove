dce1579d5fccaf60bb6b5a944f20023e17c51672
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-10-17 11:13:29 +0100

    Generate user auth module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './package/user/user.module';
 import { DatabaseModule } from './config/database/database.module';
 import { EncryptionModule } from './package/encryption/encryption.module';
+import { AuthModule } from './package/auth/auth.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule, EncryptionModule],
+  imports: [UserModule, DatabaseModule, EncryptionModule, AuthModule],
   controllers: [],
   providers: [],
 })
