bf837846f5da0e8c915a9850c53cb1cca9a32b15
sr.teppei <jesus.hernanz.mateo@gmail.com>
2020-10-18 12:24:25 +0100

    Fix errors in mysql script

--- sql/structure/v0.0.2.sql
+++ sql/structure/v0.0.2.sql
@@ -1,6 +1,7 @@
-CREATE TABLE `contact_book` {
-  `contact_book_id` INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `contact_book` (
+  `contact_book_id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `contact_name` VARCHAR(200) NOT NULL,
-  `phone` INT(9) NOT NULL
-}
+  `phone` INT(9) NOT NULL,
+  CONSTRAINT `fk_contact_book_user` FOREIGN KEY (user_id) REFERENCES user (user_id)
+);
